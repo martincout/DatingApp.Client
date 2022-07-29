4b6afbf18cf6b8675f47f4af4ef4df086dbf105f
MartinCout <martincoutinho00@gmail.com>
2022-07-29 16:19:02 -0300

    Testing errors

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { ListsComponent } from './components/lists/lists.component';
 import { MemberDetailComponent } from './components/members/member-detail/member-detail.component';
 import { MemberListComponent } from './components/members/member-list/member-list.component';
 import { MessagesComponent } from './components/messages/messages.component';
+import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
 import { AuthGuard } from './_guards/auth.guard';
 
 const routes: Routes = [
@@ -20,7 +21,7 @@ const routes: Routes = [
       {path:'messages',component:MessagesComponent},
     ]
   },
-
+  {path:'errors', component:TestErrorsComponent},
   {path:'**',component:HomeComponent, pathMatch:'full'},
 ];
 
---
+++ src/app/errors/shared.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TestErrorsComponent } from './test-errors/test-errors.component';
+
+
+
+@NgModule({
+  declarations: [
+    TestErrorsComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
